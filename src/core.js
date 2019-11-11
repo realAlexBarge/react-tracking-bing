982f78c68443ba2953994aa24c271e3d177f6895
Alexander Barge <alexander.barge@gmail.com>
2019-11-11 10:03:41 +0100

    add try catch surroundings

--- src/core.js
+++ src/core.js
@@ -10,7 +10,12 @@ export function getTracker() {
 }
 
 export function trackPageview() {
-  getTracker().push('pageLoad');
+  try {
+    getTracker().push('pageLoad');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn(e);
+  }
 }
 
 export function trackEvent(
@@ -19,34 +24,44 @@ export function trackEvent(
   eventLabel = '',
   eventValue = 0,
 ) {
-  getTracker().push('event', eventAction, {
-    event_category: eventCategory,
-    event_label: eventLabel,
-    event_value: parseInt(eventValue, 10),
-  });
+  try {
+    getTracker().push('event', eventAction, {
+      event_category: eventCategory,
+      event_label: eventLabel,
+      event_value: parseInt(eventValue, 10),
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn(e);
+  }
 }
 
 export function initialize(newTrackerId) {
-  /* eslint-disable */
-  (function(w, d, t, r, u) {
-    var f, n, i;
-    (w[u] = w[u] || []),
-      (f = function() {
-        var o = { ti: newTrackerId };
-        (o.q = w[u]), (w[u] = new UET(o)), w[u].push('pageLoad');
-      }),
-      (n = d.createElement(t)),
-      (n.src = r),
-      (n.async = 1),
-      (n.onload = n.onreadystatechange = function() {
-        var s = this.readyState;
-        (s && s !== 'loaded' && s !== 'complete') ||
-          (f(), (n.onload = n.onreadystatechange = null));
-      }),
-      (i = d.getElementsByTagName(t)[0]),
-      i.parentNode.insertBefore(n, i);
-  })(window, document, 'script', '//bat.bing.com/bat.js', 'uetq');
-  /* eslint-enable */
+  try {
+    /* eslint-disable */
+    (function(w, d, t, r, u) {
+      var f, n, i;
+      (w[u] = w[u] || []),
+        (f = function() {
+          var o = { ti: newTrackerId };
+          (o.q = w[u]), (w[u] = new UET(o)), w[u].push('pageLoad');
+        }),
+        (n = d.createElement(t)),
+        (n.src = r),
+        (n.async = 1),
+        (n.onload = n.onreadystatechange = function() {
+          var s = this.readyState;
+          (s && s !== 'loaded' && s !== 'complete') ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+        (i = d.getElementsByTagName(t)[0]),
+        i.parentNode.insertBefore(n, i);
+    })(window, document, 'script', '//bat.bing.com/bat.js', 'uetq');
+    /* eslint-enable */
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn(e);
+  }
 }
 
 export default {
